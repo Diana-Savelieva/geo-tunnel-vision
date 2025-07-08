a77c4019be4a6aab1c5878024f456dabaee77e64
Diana-Savelieva <kanyginadiana@yandex.ru>
2025-07-08 17:05:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",  // если сайт на корне собственного домена
+  base: '/pgteh.ru/',
   server: {
     host: "::",
     port: 8080,
